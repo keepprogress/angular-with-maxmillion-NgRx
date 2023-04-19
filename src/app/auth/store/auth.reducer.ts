2d07bf95f2f7b85a890a58b4d51633e92c2d72e0
Nick.Hsu <Nick.Hsu@testritegroup.com>
2023-04-19 17:30:41 +0800

    Sec 25. 371. One Root State

---
+++ src/app/auth/store/auth.reducer.ts
@@ -0,0 +1,13 @@
+import { User } from '../user.model';
+
+export interface State　{
+  user: User;
+}
+
+const initialState = {
+  user: null,
+};
+
+export function authReducer(state = initialState, action) {
+  return state;
+}
