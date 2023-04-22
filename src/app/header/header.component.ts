03f74943f0fed35ec0964e8dd5693e2b228005e4
keepprogress <keepprogress@gmail.com>
2023-04-22 15:45:16 +0800

    Sec 25. 374. Auth Finished (For Now ...)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,9 @@ import { Subscription } from 'rxjs';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
-
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import * as fromApp from '../store/app.reducer';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
@@ -14,11 +16,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   constructor(
     private dataStorageService: DataStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store.select('auth').pipe(map(authState => {
+      return authState.user;
+    })).subscribe(user => {
       this.isAuthenticated = !!user;
       console.log(!user);
       console.log(!!user);
