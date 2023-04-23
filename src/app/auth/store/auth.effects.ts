2ea7303bb8ae6790b0b6fa770ed79f654fc1e5eb
keepprogress <keepprogress@gmail.com>
2023-04-23 10:24:11 +0800

    Sec 25. 378. Important: Avoid @Effect.

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,8 +1,9 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
 
 export class AuthEffects {
-  authLogin = this.authLogin.pipe(
-    ofType(AuthActions.LOGIN_START));
+  authLogin = createEffect(this.authLogin.pipe(
+    ofType(AuthActions.LOGIN_START))
+  );
   constructor(private actions: Actions) {}
 }
