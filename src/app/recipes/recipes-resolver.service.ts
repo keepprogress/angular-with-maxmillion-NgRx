cadbc9520b110408216fb571c372f6d8545baded
keepprogress <keepprogress@gmail.com>
2023-05-28 16:02:24 +0800

    Sec 25. 406. Update, Delete and Add Recipes

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -12,7 +12,8 @@ import { RecipeService } from './recipe.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as RecipesActions from '../recipes/store/recipe.actions';
-import { take } from 'rxjs/operators';
+import { map, switchMap, take } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
@@ -31,6 +32,23 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
     // } else {
     //   return recipes;
     // }
+    this.store.select('recipes').pipe(
+      take(1),
+      map(recipesState => {
+      return recipesState.recipes;
+      }),
+      switchMap(recipes => {
+        if (recipes.length === 0) {
+          this.store.dispatch(new RecipesActions.FetchRecipes());
+          return this.actions.pipe(
+            ofType(RecipesActions.SET_RECIPES),
+            take(1)
+          );
+        } else {
+          return of(recipes);
+        }
+      })
+    );
     this.store.dispatch(new RecipesActions.FetchRecipes());
     return this.actions.pipe(ofType(RecipesActions.SET_RECIPES), take(1));
   }
